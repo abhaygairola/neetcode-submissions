6e0e3f6b58c6ef40b5adcb318cee5fcbc3d73546
a <abhaygairola8@gmail.com>
2026-08-16 18:27:04 +0530

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        if (n1 > n2) {
+            return false;
+        }
+        int l = 0;
+        int r = n1 - 1;
+        int[] f1 = new int[26];
+        int[] f2 = new int[26];
+        int matches = 0;
+
+        for (int i = 0; i < n1; i++) {
+            f1[s1.charAt(i) - 'a']++;
+            f2[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (f1[i] == f2[i]) {
+                matches++;
+            }
+        }
+                    if (matches == 26) {
+                return true;
+            }
+
+        for (r = n1; r < n2; r++) {
+            int rc = s2.charAt(r) - 'a';
+            f2[rc]++;
+            if (f1[rc] == f2[rc]) {
+                matches++;
+            } else if (f1[rc] == f2[rc] - 1) {
+                matches--;
+            }
+
+            int lc = s2.charAt(l) - 'a';
+            f2[lc]--;
+            if (f1[lc] == f2[lc]) {
+                matches++;
+            } else if (f1[lc] == f2[lc] + 1) {
+                matches--;
+            }
+            l++;
+            if (matches == 26) {
+                return true;
+            }
+        }
+
+        return false; // checking last window
+    }
+}
