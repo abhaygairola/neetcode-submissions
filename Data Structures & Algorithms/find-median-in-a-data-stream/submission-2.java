d0201db422fe6f46723a40bc2ed8160247eb15dc
a <abhaygairola8@gmail.com>
2026-08-31 22:54:58 +0530

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.java
@@ -0,0 +1,34 @@
+class MedianFinder {
+    PriorityQueue<Integer> small;
+    PriorityQueue<Integer> large;
+
+    public MedianFinder() {
+        small = new PriorityQueue<>(Collections.reverseOrder());
+        large = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        small.offer(num);
+
+        if (small.size() > large.size() + 1 || (!large.isEmpty() && small.peek() > large.peek())) {
+            large.offer(small.poll());
+        }
+        if (large.size() > small.size() + 1) {
+            small.offer(large.poll());
+        }
+    }
+
+    public double findMedian() {
+        int s = small.size();
+        int l = large.size();
+        int ss = small.peek() == null ? 0 : small.peek();
+        int ll = large.peek() == null ? 0 : large.peek();
+        if (l == s) {
+            return (ss + ll) / 2.0;
+        } else if (l > s) {
+            return ll;
+        } else {
+            return ss;
+        }
+    }
+}
