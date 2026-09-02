1b7cd896df7185a8786912f859bd40e9af7f01a2
a <abhaygairola8@gmail.com>
2026-09-02 17:19:47 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        res.add(cur);
+        dfs(nums, res, cur, 0);
+        return res;
+    }
+
+    void dfs(int[] nums, List<List<Integer>> res, List<Integer> cur, int i) {
+        if (i >= nums.length)
+            return;
+        cur.add(nums[i]);
+        res.add(new ArrayList<>(cur));
+        dfs(nums, res, cur, i + 1);
+        cur.remove(cur.size() - 1);
+        dfs(nums, res, cur, i + 1);
+    }
+}
