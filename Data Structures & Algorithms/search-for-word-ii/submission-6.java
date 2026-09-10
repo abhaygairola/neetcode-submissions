a24f4ba697c4445142ff1cd990c592dcf82ba19c
a <abhaygairola8@gmail.com>
2026-09-10 13:59:47 +0530

    Add: search-for-word-ii - submission-6

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-6.java
@@ -0,0 +1,52 @@
+class Solution {
+    class TrieNode {
+        HashMap<Character, TrieNode> children;
+        boolean end;
+
+        public TrieNode() {
+            end = false;
+            children = new HashMap<>();
+        }
+    }
+    TrieNode root;
+    public List<String> findWords(char[][] board, String[] words) {
+        root = new TrieNode();
+        List<String> res = new ArrayList<>();
+        for (String word : words) {
+            TrieNode cur = root;
+            for (char c : word.toCharArray()) {
+                if (!cur.children.containsKey(c)) {
+                    cur.children.put(c, new TrieNode());
+                }
+                cur = cur.children.get(c);
+            }
+            cur.end = true;
+        }
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                dfs(board, r, c, res, "", root);
+            }
+        }
+        return res;
+    }
+
+    void dfs(char[][] board, int r, int c, List<String> res, String path, TrieNode cur) {
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] == '.'
+            || !cur.children.containsKey(board[r][c])) {
+            return;
+        }
+        String s = path + board[r][c];
+        cur = cur.children.get(board[r][c]);
+        if (cur.end) {
+            res.add(s);
+            cur.end = false; // important: avoid duplicates
+        }
+        char temp = board[r][c];
+        board[r][c] = '.';
+        dfs(board, r + 1, c, res, s, cur);
+        dfs(board, r - 1, c, res, s, cur);
+        dfs(board, r, c + 1, res, s, cur);
+        dfs(board, r, c - 1, res, s, cur);
+        board[r][c] = temp;
+    }
+}
