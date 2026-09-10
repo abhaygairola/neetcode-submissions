4631c2c6dc28903bb9da14bd46a9e7725c910d33
a <abhaygairola8@gmail.com>
2026-09-10 17:49:30 +0530

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    // public int reverseBits(int n) {
+    //     int r = 0,b=0;
+    //     for (int i = 0; i < 32; i++) {
+    //         b = ((n >>> i) & 1) << (31-i);
+    //         r |= b;
+    //     }
+    //     return r;
+    // }
+    public int reverseBits(int n) {
+        n = n >>> 16 | n << 16; // 16
+        n = (n & 0xff00ff00) >>> 8 | (n & 0x00ff00ff) << 8; // 8
+        n = (n & 0xf0f0f0f0) >>> 4 | (n & 0x0f0f0f0f) << 4; // 4
+        n = (n & 0xcccccccc) >>> 2 | (n & 0x33333333) << 2; // 2
+        n = (n & 0xaaaaaaaa) >>> 1 | (n & 0x55555555) << 1; // 1
+        return n;
+    }
+}
